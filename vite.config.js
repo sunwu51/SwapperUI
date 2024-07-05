44e037d0da46114a22347a59caf09fad89a5b3a2
Frank <sunwu51@126.com>
2024-07-05 11:21:50 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,14 @@ export default defineConfig({
     alias: {
       '@': path.resolve('./src')
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
   }
 })
