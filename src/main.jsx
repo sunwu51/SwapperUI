60bfa90a3ef672f4e8c838bf27705dc785fc7624
Frank <sunwu51@126.com>
2025-02-08 12:04:12 +0000

    merge monaco to self

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,36 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { loader } from '@monaco-editor/react';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+import * as monaco from 'monaco-editor';
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+
+self.MonacoEnvironment = {
+  getWorker(_, label) {
+    if (label === 'json') {
+      return new jsonWorker();
+    }
+    if (label === 'css' || label === 'scss' || label === 'less') {
+      return new cssWorker();
+    }
+    if (label === 'html' || label === 'handlebars' || label === 'razor') {
+      return new htmlWorker();
+    }
+    if (label === 'typescript' || label === 'javascript') {
+      return new tsWorker();
+    }
+    return new editorWorker();
+  },
+};
+
+loader.config({ monaco });
+
+loader.init().then(()=>{
+    ReactDOM.createRoot(document.getElementById('root')).render( <App /> )
+})
+
